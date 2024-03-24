b55e47407945fffafb50d1a474eda0d07d1afa05
mishio-n <doncool340@gmail.com>
2024-03-24 11:06:17 +0900

    update

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,13 +1,13 @@
 /// <reference types="@types/serviceworker" />
-import PQueue from 'p-queue';
+// import PQueue from 'p-queue';
 
-import { jitter } from './jitter';
-import { transformJpegXLToBmp } from './transformJpegXLToBmp';
+// import { jitter } from './jitter';
+// import { transformJpegXLToBmp } from './transformJpegXLToBmp';
 
-// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
-const queue = new PQueue({
-  concurrency: 5,
-});
+// // ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
+// const queue = new PQueue({
+//   concurrency: 5,
+// });
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -17,23 +17,23 @@ self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(
-    queue.add(() => onFetch(ev.request), {
-      throwOnTimeout: true,
-    }),
-  );
-});
+// self.addEventListener('fetch', (ev: FetchEvent) => {
+//   ev.respondWith(
+//     queue.add(() => onFetch(ev.request), {
+//       throwOnTimeout: true,
+//     }),
+//   );
+// });
 
-async function onFetch(request: Request): Promise<Response> {
-  // サーバーの負荷を分散するために Jitter 処理をいれる
-  await jitter();
+// async function onFetch(request: Request): Promise<Response> {
+//   // サーバーの負荷を分散するために Jitter 処理をいれる
+//   await jitter();
 
-  const res = await fetch(request);
+//   const res = await fetch(request);
 
-  if (res.headers.get('Content-Type') === 'image/jxl') {
-    return transformJpegXLToBmp(res);
-  } else {
-    return res;
-  }
-}
+//   if (res.headers.get('Content-Type') === 'image/jxl') {
+//     return transformJpegXLToBmp(res);
+//   } else {
+//     return res;
+//   }
+// }
