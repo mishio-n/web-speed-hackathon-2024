89eb45ffcaeb1a85848db3eef43429f3d5f19ef4
mishio-n <doncool340@gmail.com>
2024-03-24 12:30:00 +0900

    aタグの遷移をやめる

--- workspaces/app/src/foundation/components/Link.tsx
+++ workspaces/app/src/foundation/components/Link.tsx
@@ -1,12 +1,21 @@
+import { forwardRef } from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
 type Props = {
   children: React.ReactNode;
   to?: string;
 } & JSX.IntrinsicElements['a'];
 
-export const Link: React.FC<Props> = ({ children, to, ...rest }) => {
-  return (
-    <a href={to} {...rest}>
+// eslint-disable-next-line react/display-name
+export const Link = forwardRef<HTMLAnchorElement, Props>(({ children, to, ...rest }, ref) => {
+  return to === undefined ? (
+    <a ref={ref} {...rest}>
       {children}
     </a>
+  ) : (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    <ReactRouterLink ref={ref as any} to={to} {...rest}>
+      {children}
+    </ReactRouterLink>
   );
-};
+});
